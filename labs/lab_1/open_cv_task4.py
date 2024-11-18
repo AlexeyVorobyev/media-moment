e7fb007a59f76f2375828dbe32d7b1b21f55ebe6
Alexey Vorobyev <a.vorobyev@winsolutions.ru>
2024-11-19 01:19:15 +0300

    LAB 1 TASK 4
    Выполнено задание 4
       - Записано видео из одного файла в другой файл

---
+++ labs/lab_1/open_cv_task4.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Название входного файла
+input_file = "files/4_input.mp4"
+# Название выходного файла
+output_file = "files/4_output.mp4"
+
+# Открываем входной видеофайл
+cap = cv2.VideoCapture(input_file)
+
+# Получаем параметры входного видео
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Создаем выходной видеофайл
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+# Читаем и записываем кадры
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    out.write(frame)
+
+# Освобождаем ресурсы
+cap.release()
+out.release()
