5fa959930d24182c4fc4ae05110354c0066016ac
Alexey Vorobyev <a.vorobyev@winsolutions.ru>
2024-11-19 16:04:33 +0300

    LAB 2 TASK 1
    Прочитал изображение с камеры и перевёл его в формат
    HSV

---
+++ labs/lab_2/helper.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def begin():
+    return cv2.VideoCapture(0)
+
+
+def process_stream(cap, process_frame_callback):
+    while True:
+        ret, frame = cap.read()  # Чтение текущего кадра
+        if not ret:  # Проверка на успешное чтение кадра
+            break
+
+        process_frame_callback(frame)
+
+        # Ожидание 20 мс и проверка нажатия клавиши 'Esc' (код 27)
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+
+def end(cap):
+    cap.release()
+    cv2.destroyAllWindows()
