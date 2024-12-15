a3ef443f4e5cd55ff72f5fa196b5a1b3b2f44491
Alexey Vorobyev <a.vorobyev@winsolutions.ru>
2024-12-15 22:37:49 +0300

    LAB 7 инициализация, метрики сходства, базовая модель

---
+++ labs/lab_7/val_type/FullValType.py
@@ -0,0 +1,7 @@
+from BaseValType import BaseValType
+
+
+class FullValType(BaseValType):
+
+    def check_value(self, model_value: str, correct_value: str) -> float | int:
+        return 1 if model_value == correct_value else 0
